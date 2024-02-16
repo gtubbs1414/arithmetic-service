14ed8c112bead62c869c26bde075a76383d3404f
Gannon Tubbs <tubbsgannon@gmail.com>
2024-02-15 19:24:14 -0500

    docker update

--- src/test/arithmetica.test.js
+++ src/test/arithmetica.test.js
@@ -2,4 +2,13 @@ const { add } = require("../arithmetica")
 
 test('2 + 3 is equal to 5', () => {
     expect(add(2, 3)).toBe(5);
-})
+});
+
+test('100 + 100 is 200', () => {
+    expect(add(100, 100)).toBe(200);
+});
+
+
+test('250 + 100 is 350', () => {
+    expect(add(250, 100)).toBe(350);
+});
