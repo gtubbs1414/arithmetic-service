9ed2d610c2eb19b3d0a4bc39414ed6ff0b0b167a
Gannon Tubbs <tubbsgannon@gmail.com>
2024-03-05 19:40:46 -0500

    practicum update

--- src/index.js
+++ src/index.js
@@ -1,42 +1,42 @@
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const { add } = require("./arithmetica");
 const app = express();
 app.use(cors());
-const port = 3000;
+// const port = 3000;
 
+// if (!process.env.PORT) {
+//   throw new Error("please specify port number");
+// }
 
-app.get('/', (req, res) => {
-    // sends html
-    res.send('Arithmetic Service - Hello World!');
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  // sends html
+  res.send("Arithmetic Service - last updated 3/4/2022");
 });
 
-app.get('/add/:n/:m', (req, res) => {
+app.get("/add/:n/:m", (req, res) => {
+  let n = Number(req.params.n);
+  let m = Number(req.params.m);
+  let sum = add(n, m);
 
-    let n = Number(req.params.n);
-    let m = Number(req.params.m);
-    let sum = add(n, m);
-        
-    res.json(sum);
+  res.json(sum);
 
-    // res.json(Number(req.params.n) + Number(req.params.m));
+  // res.json(Number(req.params.n) + Number(req.params.m));
 });
 
-app.get('/subtract/:n/:m', (req, res) => {
-
-    res.json(Number(req.params.n) - Number(req.params.m));
+app.get("/subtract/:n/:m", (req, res) => {
+  res.json(Number(req.params.n) - Number(req.params.m));
 });
 
-app.get('/multiply/:n/:m', (req, res) => {
-
-    res.json(Number(req.params.n) * Number(req.params.m));
+app.get("/multiply/:n/:m", (req, res) => {
+  res.json(Number(req.params.n) * Number(req.params.m));
 });
 
-app.get('/divide/:n/:m', (req, res) => {
-
-    res.json(Number(req.params.n) / Number(req.params.m));
+app.get("/divide/:n/:m", (req, res) => {
+  res.json(Number(req.params.n) / Number(req.params.m));
 });
-
-
 
 app.listen(port);
